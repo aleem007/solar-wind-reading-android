1179a34bc3882b787b7aedaefa5dcf504de9a742
sandeep cmsm <sandeep.cmsm@hotmail.com>
2016-12-31 00:17:05 +0530

    adding layouts.

--- app/src/main/java/com/emwios/solarwind/MainActivity.java
+++ app/src/main/java/com/emwios/solarwind/MainActivity.java
@@ -1,11 +1,8 @@
 package com.emwios.solarwind;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,15 +13,6 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
-      }
-    });
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,4 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
     return super.onOptionsItemSelected(item);
   }
+
+/*  mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+    @Override public boolean onNavigationItemSelected (@NonNull MenuItem item){
+    // handle desired action here
+    // One possibility of action is to replace the contents above the nav bar
+    // return true if you want the item to be displayed as the selected item
+    return true;
+  }
+  }*/
 }
